2b9ac44fa215cca4b7189b809cdb4119c3b4231d
nywsec <nyws@protonmail.com>
2020-03-27 23:12:15 +0100

    shodan 3

--- tools/shodan/main.go
+++ tools/shodan/main.go
@@ -10,17 +10,23 @@ import (
 )
 
 func main() {
-
-	orgCmd := flag.NewFlagSet("org", flag.ExitOnError)
+	// Organisation search
+	orgCmd := flag.NewFlagSet("org", flag.ContinueOnError)
 	orgGet := orgCmd.String("h", "", "Usage: -h ORG. Retrieved information about target Organisation")
 
+	// Domain name
 	dnsreq := flag.NewFlagSet("dns", flag.ExitOnError)
 	dns := dnsreq.String("d", "", "Usage: -dns 'Domain Name'")
+
+	//Dns Reverse resolution
 
 	if len(os.Args) < 2 {
 		log.Fatalln("Usage: shodan Searchterm")
 		os.Exit(1)
 	}
+
+	ipCmd := flag.NewFlagSet("ip", flag.ExitOnError)
+	ipGet := ipCmd.String("h", "", "Usage: ip -h 1.2.3.4")
 
 	//fmt.Println(*dns)
 	apiKey := os.Getenv("SHODAN_API_KEY")
@@ -39,33 +45,66 @@ func main() {
 	case "org":
 
 		orgCmd.Parse(os.Args[2:])
-		query, err := s.HostSearch(*orgGet)
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Printf("[*] Results for target '%v'\n", *orgGet)
-		for _, host := range query.Matches {
-			//		index := len(host.Domains)
-			for _, hosting := range host.Domains {
-				fmt.Printf("IP:%18s Port:%8d Domain:%40s\n", host.IPString, host.Port, hosting)
-			}
+		OrgSearch(*orgGet)
+		if len(os.Args[2:]) == 0 || os.Args[2:] == nil {
+			//fmt.Println("Usage: org -h organisation")
+			os.Exit(1)
 		}
 
 	case "dns":
 		dnsreq.Parse(os.Args[2:])
-		hostlist := make([]string, 0)
-		hostlist = append(hostlist, *dns)
-		dsearch, err := s.DnsSearch(hostlist)
-		time.Sleep(2 * time.Second)
+		Dns(*dns)
+		if len(os.Args[2:]) <= 1 {
+			//fmt.Println("Usage: org -h organisation")
+			os.Exit(1)
+		}
+
+	case "ip":
+		ipCmd.Parse(os.Args[2:])
+		query, err := s.HostIP(*ipGet)
 		if err != nil {
-			fmt.Println(err)
+			log.Fatalln(err)
 		}
-		for _, v := range dsearch {
-			fmt.Printf("DNS Resolution  : %v\n", v)
-		}
+		//for _, v := range query.Data {
+		//	fmt.Printf("Ip %v", v)
+		//}
+		fmt.Printf("Hosting %v\n", query.Data[len(query.Data)-1])
 
 	default:
-		fmt.Println("Error")
+		fmt.Println("Error parsing options ! Exiting ...")
 	}
 
+}
+
+func OrgSearch(org string) {
+
+	apiKey := os.Getenv("SHODAN_API_KEY")
+	s := shodan.New(apiKey)
+	query, err := s.HostSearch(org)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("[*] Results for target '%v'\n\n", org)
+	for _, host := range query.Matches {
+		//		index := len(host.Domains)
+		for _, hosting := range host.Domains {
+			fmt.Printf("IP:%18s Port:%8d Hosting:%30s Location: %v\n", host.IPString, host.Port, hosting, host.Location)
+		}
+	}
+}
+
+func Dns(dns string) {
+
+	apiKey := os.Getenv("SHODAN_API_KEY")
+	s := shodan.New(apiKey)
+	hostlist := make([]string, 0)
+	hostlist = append(hostlist, dns)
+	dsearch, err := s.DnsSearch(hostlist)
+	time.Sleep(2 * time.Second)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, v := range dsearch {
+		fmt.Printf("DNS Resolution  : %v\n", v)
+	}
 }
---
+++ tools/shodan/shodan/ip.go
@@ -0,0 +1,64 @@
+package shodan
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type HostIP struct {
+	RegionCode   string      `json:"region_code"`
+	IP           int         `json:"ip"`
+	AreaCode     int         `json:"area_code"`
+	Latitude     float64     `json:"latitude"`
+	Hostnames    []string    `json:"hostnames"`
+	PostalCode   string      `json:"postal_code"`
+	DmaCode      int         `json:"dma_code"`
+	CountryCode  string      `json:"country_code"`
+	Org          string      `json:"org"`
+	Data         []Data      `json:"data"`
+	City         string      `json:"city"`
+	Isp          string      `json:"isp"`
+	Longitude    float64     `json:"longitude"`
+	LastUpdate   string      `json:"last_update"`
+	CountryCode3 string      `json:"country_code3"`
+	CountryName  string      `json:"country_name"`
+	IPStr        string      `json:"ip_str"`
+	Os           interface{} `json:"os"`
+	Ports        []int       `json:"ports"`
+}
+
+type Data struct {
+	Product string `json:"product"`
+	Title   string `json:"title"`
+
+	//Timestamp string   `json:"timestamp"`
+	Isp  string   `json:"isp"`
+	Cpe  []string `json:"cpe"`
+	Data string   `json:"data"`
+	HTML string   `json:"html"`
+
+	IP        int         `json:"ip"`
+	Domains   []string    `json:"domains"`
+	Org       string      `json:"org"`
+	Os        interface{} `json:"os"`
+	Port      int         `json:"port"`
+	Hostnames []string    `json:"hostnames"`
+	IPStr     string      `json:"ip_str"`
+}
+
+func (c *Client) HostIP(ip string) (*HostIP, error) {
+
+	h := &HostIP{}
+	//https://api.shodan.io/shodan/host/{ip}?key={YOUR_API_KEY}
+	resp, err := http.Get(fmt.Sprintf("%s/shodan/host/%s?key=%s", BaseURL, ip, c.apiKey))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer resp.Body.Close()
+	if err := json.NewDecoder(resp.Body).Decode(&h); err != nil {
+		return nil, err
+	}
+	return h, nil
+}
